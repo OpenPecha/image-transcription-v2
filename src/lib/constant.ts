41dddf262808b4d3b3113c0607a02bd6ad1f3cc5
Jampaa <106014359+Jampaa@users.noreply.github.com>
2026-06-15 16:48:12 +0530

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,4 +2,5 @@ export const APPLICATION_NAME = 'imagetranscriptionv2'
 
 export const APPLICATION_URLS: Record<string, string> = {
   scriptclassification: 'https://script-classification.onrender.com',
+  imagetranscription: 'https://image-transcription.onrender.com',
 }
